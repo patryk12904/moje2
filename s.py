7340c3c6c8a9499d8e7f4a3f4d113ab41b6c050a
patryk12904 <patrykos12904@wp.pl>
2018-09-30 14:19:05 +0200

    fgd

---
+++ s.py
@@ -0,0 +1,5 @@
+a = input("Podaj 1 ulubiony przysmak: \n")
+b = input("Podaj 2 ulubiony przysmak: \n")
+
+print("ublubione przysmaki to: \n",a+" "+b)
+
---
+++ zad2.py
@@ -0,0 +1,3 @@
+a = int(input("podaj kwote rachunku \n"))
+print("Napiwek w wysokosci 15%: \n", a*0.15)
+print("Napiwek w wysokosci 20%: \n", a*0.20)
